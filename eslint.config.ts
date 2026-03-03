83341768633294ce74b891597f25149605c82ae2
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-03 21:42:57 +0700

    feat(configs): add tailwindcss and its eslint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,10 +3,13 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import eslintParserAstro from 'astro-eslint-parser';
+import { parser as eslintParserTypeScript } from 'typescript-eslint';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ['**/*.{mjs,ts,tsx,astro}'],
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
@@ -15,6 +18,27 @@ export default defineConfig([
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs['jsx-a11y-strict'],
   {
-    rules: {},
+    files: ['**/*.{tsx,astro}'],
+    extends: [eslintPluginBetterTailwindcss.configs.recommended],
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/styles/tailwind/index.css',
+      },
+    },
+    rules: {
+      'better-tailwindcss/enforce-consistent-variable-syntax': 'warn',
+      'better-tailwindcss/enforce-consistent-important-position': 'warn',
+      'better-tailwindcss/enforce-shorthand-classes': 'warn',
+    },
+  },
+  {
+    files: ['**/*.{astro}'],
+
+    languageOptions: {
+      parser: eslintParserAstro,
+      parserOptions: {
+        parser: eslintParserTypeScript,
+      },
+    },
   },
 ]);
