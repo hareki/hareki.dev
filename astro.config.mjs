7b21403ecb3ff2d7988d879cf33275810ce0f30d
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-05 20:22:19 +0700

    feat(config): integrate @playform/compress for better compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    playformCompress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+    }),
+  ],
 });
+
