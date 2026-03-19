a21e1b05b19c0dd53c9a0afc39bc3d446fd47000
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-19 16:01:49 +0700

    feat(terminal): show local date/time in recent commit tooltip with UTC data attribute

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 import { format, formatDistanceToNowStrict } from 'date-fns';
 
 export const getFormattedDate = (date: Date) => format(date, 'dd MMM yyyy');
+export const getFormattedDateTime = (date: Date) =>
+  format(date, 'dd MMM yyyy, h:mm a O');
 export const getDateDistance = (date: Date) =>
-  formatDistanceToNowStrict(date, {
-    addSuffix: true,
-  });
+  formatDistanceToNowStrict(date, { addSuffix: true });
