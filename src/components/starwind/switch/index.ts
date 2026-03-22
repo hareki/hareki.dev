d86b130d4a7de0ab109829c3be798101e46bc060
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-22 20:13:13 +0700

    feat: add starwind Switch

---
+++ src/components/starwind/switch/index.ts
@@ -0,0 +1,17 @@
+import Switch, {
+  switchButton,
+  switchLabel,
+  switchToggle,
+} from './Switch.astro';
+
+import type { SwitchChangeEvent } from './SwitchTypes';
+
+const SwitchVariants = {
+  switchButton,
+  switchToggle,
+  switchLabel,
+};
+
+export { Switch, type SwitchChangeEvent, SwitchVariants };
+
+export default Switch;
