1fa6cc4268d299544984363cecf67825e46f8f29
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-29 10:14:39 +0700

    fix: refine easterEggLog's wording

--- src/layouts/main-layout/utils.ts
+++ src/layouts/main-layout/utils.ts
@@ -3,8 +3,12 @@ import { PROJECT_REPO_FULL_NAME } from '@/data/git';
 export const easterEggLog = () => {
   const separator = `%c`;
   console.log(
-    `${separator}\n%c👋 Greetings! Interested in how the site is built? 👀
-\n%cCheck out the source code: https://github.com/${PROJECT_REPO_FULL_NAME}\n%cHave a great day!\n${separator}`,
+    `${separator}
+%cGreetings! Interested in how this website is built?
+
+%cCheck out the source code: https://github.com/${PROJECT_REPO_FULL_NAME}
+%cHave a great day!
+${separator}`,
     '',
     'font-size:1rem;font-weight:700;',
     'font-size: 0.75rem;font-weight:700',
