0c6f16ff533cb6ea0cafeb319b406a9775b47621
Tu Nguyen <minhtu1392000@gmail.com>
2026-03-30 14:52:15 +0700

    refactor: use Astro font api instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,47 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, envField } from 'astro/config';
-
+import { defineConfig, envField, fontProviders } from 'astro/config';
 import playformCompress from '@playform/compress';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import { cloudflareCdnCache } from './src/lib/cache/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hareki.dev',
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Maple Mono',
+      fallbacks: ['monospace'],
+      cssVariable: '--font-maple-mono',
+      options: {
+        /* 
+        Generated with the following command, basic latin characters with "ễ" letter for the HeroSection
+        pyftsubset ./MapleMonoNL-<FONT_WEIGHT>.ttf.woff2 \
+          --output-file="./MapleMono-<FONT_WEIGHT>.woff2" \
+          --flavor=woff2 \
+          --layout-features="kern,calt" \
+          --unicodes="U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD,U+1EC5" \
+          --no-hinting \
+          --desubroutinize 
+        */
+        variants: [
+          {
+            src: ['./src/assets/fonts/MapleMono-Medium.woff2'],
+            weight: 'normal',
+            style: 'normal',
+          },
+
+          {
+            src: ['./src/assets/fonts/MapleMono-Bold.woff2'],
+            weight: 'bold',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
   build: {
     // PERF: This resource is render blocking anyway, inline it to avoid round trips
     inlineStylesheets: 'always',
